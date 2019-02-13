607bb3918265e18f26fb2135df7526868f1c3ce5
YuYi <18058527586@163.com>
2019-02-14 02:31:41 +0800

    补提示框

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="login-wrap">
-        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-            <h2>用户登录</h2>
-            <el-form-item label="用户名">
-                <el-input v-model="formdata.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="formdata.password"></el-input>
-            </el-form-item>
-            <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登录</el-button>
-        </el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -28,9 +28,11 @@ export default {
       this.$http.post(`login`, this.formdata).then(res => {
         const { data: { data, meta: { msg, status } } } = res;
         if (status === 200) {
-          console.log("login~~success~~");
+          this.$router.push({
+            name: "home"
+          });
         } else {
-          console.log('err___________');
+          this.$message.error(msg);
         }
       });
     }
